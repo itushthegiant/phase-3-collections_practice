47e90cfb6e34c8fbd20ccb57fd00acf901f530d5
etamar <etamar.levy@gmail.com>
2021-07-25 13:37:40 -0400

    commit tree e3f03b928acfa39fdc9bf4014549a3ad81bb3b79

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,60 @@
+def sort_array_asc(array)
+    array.sort
+end
 
+
+def sort_array_desc(array)
+    array.sort do |left, right|
+        right <=> left
+    end
+end
+
+
+def sort_array_char_count(array)
+    array.sort do |left, right|
+        left.length <=> right.length
+    end
+end
+
+
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+
+
+def reverse_array(array)
+    array.reverse
+end
+
+
+def kesha_maker(array)
+    array.each do |name|
+        name[2] = "$"
+    end
+end
+
+
+def find_a(array)
+    array.filter do |string|
+        string[0] == "a"
+    end
+end
+
+
+def sum_array(array)
+    array.reduce(:+)
+end
+
+
+def add_s(array)
+    array.map.with_index do |element, index|
+        if index == 1
+            element
+        else
+            element + "s"
+        end
+    end
+end
